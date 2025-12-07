45f1b7e33253da717217ddd0d1b6c23c916225e9
NitishKumar-art <nitish.kr16102002@gmail.com>
2025-12-07 15:20:25 +0530

    dashboard

---
+++ dashboard/src/components/Apps.js
@@ -0,0 +1,7 @@
+import React,{ useState, useEffect, useRef} from "react";
+
+const Apps = () => {
+  return <h1>Apps</h1>;
+};
+
+export default Apps;
---
+++ dashboard/src/components/Home.js
@@ -0,0 +1,15 @@
+import React,{ useState, useEffect, useRef} from "react";
+
+import Dashboard from "./Dashboard";
+import TopBar from "./TopBar";
+
+const Home = () => {
+  return (
+    <>
+      <TopBar />
+      <Dashboard />
+    </>
+  );
+};
+
+export default Home;
